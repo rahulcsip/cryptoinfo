4f806495cf5788de8a08def5aad879945b27c349
rahucsip <rahucsip@gmail.com>
2022-02-13 01:23:50 +0530

    cryptoinfo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter , Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Info from './pages/Info'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/cryptoinfo/' element={<Home/>} />
       <Route path='/cryptoinfo/info/:id' element={<Info/>} />
+      <Route path='*' element={<NotFound/>} />
     </Routes>
     </BrowserRouter>
   )
